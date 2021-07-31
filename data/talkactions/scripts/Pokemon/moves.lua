58bbb43ba945b0b1bbbfa22ae09baa3c81af415d
najatheus1 <matheustoledo01@outlook.com>
2021-07-31 01:13:45 -0300

    Create moves.lua

---
+++ data/talkactions/scripts/Pokemon/moves.lua
@@ -0,0 +1,14 @@
+function onSay(player, words, param)
+    local slot = player:getSlotItem(CONST_SLOT_FEET)
+    if(not(slot))then
+        return
+    end
+    
+    local pokeball = slot:getAttribute(ITEM_ATTRIBUTE_POKEBALL)
+
+    local pokemon = player:getSummons()[1]
+    if(not(pokemon))then
+        return
+    end
+    
+end
