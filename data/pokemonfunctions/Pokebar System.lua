97c5c0f96709788ffd2a86d767a195c7b3118170
najatheus1 <matheustoledo01@outlook.com>
2021-08-01 18:20:09 -0300

    Update

--- data/pokemonfunctions/Pokebar System.lua
+++ data/pokemonfunctions/Pokebar System.lua
@@ -1,3 +1,10 @@
+function returnPokemonBar(player, pokeball_item)
+local pokemon = player:getSummons()[1]
+if(not(pokemon))then
+    return
+end
+    return pokemon:returnPokemon(player, pokeball_item) and pokeball_item:movePokeball(player)
+end
 function Item:movePokeball(player)
 local slot_bag = player:getSlotItem(3)
 if(not(slot_bag))then
@@ -5,11 +12,13 @@ if(not(slot_bag))then
 end
 self:moveTo(slot_bag)
 end
-
 function Pokebar(player)
 local pokeball_item = player:getSlotItem(8)
 if(not(pokeball_item))then
     return
+end
+if(#player:getSummons() >= 1)then
+    return returnPokemonBar(player, pokeball_item)
 end
 GoPokemon(player, pokeball_item)
 end
