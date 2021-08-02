676345a56fde205cd5e66de078fbd9da3a46ec6e
najatheus1 <matheustoledo01@outlook.com>
2021-08-02 02:34:22 -0300

    Update PokeInfo System.lua

--- data/pokemonfunctions/PokeInfo System.lua
+++ data/pokemonfunctions/PokeInfo System.lua
@@ -1,5 +1,5 @@
 function Player:monsterInfo(pokemon)
-local slot = self:getSlotItem(CONST_SLOT_FEET)
+--[[local slot = self:getSlotItem(CONST_SLOT_FEET)
 if not slot then return false end
 local pokeball_name = slot:getAttribute(ITEM_ATTRIBUTE_POKEBALL)
 local name_table = CONFIG_POKEMON[pokeball_name]
@@ -10,5 +10,5 @@ if not gender_table then return false end
 local pokeball_level = slot:getAttribute(ITEM_ATTRIBUTE_POKELEVEL)
 local pokeball_nature = slot:getAttribute(ITEM_ATTRIBUTE_POKENATURE)
 local nature_table = NATURE_CONFIG[pokeball_nature]
-if not nature_table then return false end
+if not nature_table then return false end]]
 end
